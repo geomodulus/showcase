2a111c38899206f11c7f17bbe95d5c97e4f14607
kylerobertduncan <kylerobertduncan@gmail.com>
2022-05-09 17:31:18 -0400

    Added municipalities to map and built basic quiz and timer framework
    Adding munc centres and labels
    
    Regions and label revealed on correct guess
    
    retrieves data from mapbox source, streamlines flyTo
    
    Added showRemaining func to lose conditions
    
    Added viz bar for time/answers

---
+++ articles/greater-toronto-area-quiz/municipal-boundaries.js
@@ -0,0 +1,54 @@
+// turn off mapbox labels on load
+const layerList = module.map
+  .getStyle()
+  .layers.filter((layer) => layer.type === "symbol");
+layerList.forEach((layer) => {
+  if (layer["source-layer"] === "place_label") {
+    module.map.setLayoutProperty(layer.id, "visibility", "none");
+  }
+});
+// put labels back on article close
+window.addEventListener(
+  "flexWindowReset",
+  () => {
+    layerList.forEach((layer) => {
+      if (layer["source-layer"] === "place_label") {
+        module.map.setLayoutProperty(layer.id, "visibility", "visible");
+      }
+    });
+  },
+  { once: true }
+);
+
+function addLayers() {
+  module.addFeatureLayer({
+    id: "boundary-fills",
+    type: "fill",
+    source: "municipal-boundaries",
+    paint: {
+      "fill-color": "#FFF", // white
+      "fill-opacity": 0.2,
+    },
+  });
+  module.addFeatureLayer({
+    id: "boundary-lines",
+    type: "line",
+    source: "municipal-boundaries",
+    paint: {
+      "line-color": "#FFF", // white
+      "line-width": 2,
+      "line-opacity": 0.2,
+    },
+  });
+}
+
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    module.addSource("municipal-boundaries", {
+      type: "geojson",
+      data: data,
+    });
+    addLayers();
+  })
+  .catch((e) => console.error(e));
